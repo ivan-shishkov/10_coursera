f205ee88d014b0fc8339ef2ca67c394460d03360
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-11 21:01:14 +0300

    Added script main function

--- coursera.py
+++ coursera.py
@@ -1,16 +1,11 @@
 
 
-def get_courses_list():
-    pass
 
 
-def get_course_info(course_slug):
-    pass
 
-
-def output_courses_info_to_xlsx(filepath):
+def main():
     pass
 
 
 if __name__ == '__main__':
-    pass
+    main()
