dab26cbb81353c3c771a2e991878fb51b86998db
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-06-14 21:27:12 +0300

    Added parsing of command line arguments

--- coursera.py
+++ coursera.py
@@ -1,10 +1,30 @@
+import argparse
 
 
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument(
+        'output',
+        help='a xlsx file to save info about Coursera courses',
+        type=str,
+    )
+    parser.add_argument(
+        '--count',
+        help='a count of randomly selected Coursera courses (default: 20)',
+        default=20,
+        type=int,
+    )
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
 
 
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
+
+    output_filepath = command_line_arguments.output
+    courses_count = command_line_arguments.count
 
 
 if __name__ == '__main__':
